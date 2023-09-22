518f4024371224981201244554b5bae3f331bb54
Nicolai <nicolai.verbaarschot@gmail.com>
2023-09-22 23:54:02 +0200

    replaced placeholder content for rendering.

--- src/routes/rendering/w01/+page.svelte
+++ src/routes/rendering/w01/+page.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
+	import type { PageData } from './$types';
+	import Part from '$lib/components/Part.svelte';
 	import Part1 from './Part1.svelte';
 	import Part2 from './Part2.svelte';
 	import Part3 from './Part3.svelte';
 	import Part4 from './Part4.svelte';
+	export let data: PageData;
 </script>
 
 <h1>Getting started with WebGL (or WebGPU)</h1>
+<Part sourceString={data.graphics.w01.Part1}>
+	<Part1 />
+</Part>
 
-<Part1 />
-<Part2 />
-<Part3 />
-<Part4 />
+<Part sourceString={data.graphics.w01.Part2}>
+	<Part2 />
+</Part>
+
+<Part sourceString={data.graphics.w01.Part3}>
+	<Part3 />
+</Part>
+<Part sourceString={data.graphics.w01.Part4}>
+	<Part4 />
+</Part>
